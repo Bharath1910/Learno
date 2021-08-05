71d260b63288a25fa0d4c435c266015d485d5b84
Bharath <av.bharathshanmugam@gmail.com>
2021-08-05 11:11:45 +0530

    More Features :eyes:
    added importing formulas from txt files and made code look kinda decent 👀

--- setup.py
+++ setup.py
@@ -1,40 +1,97 @@
-from configparser import ConfigParser
-
-ascii = """ /$$                                                        
-| $$                                                        
-| $$        /$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$$   /$$$$$$ 
-| $$       /$$__  $$ |____  $$ /$$__  $$| $$__  $$ /$$__  $$
-| $$      | $$$$$$$$  /$$$$$$$| $$  \__/| $$  \ $$| $$  \ $$
-| $$      | $$_____/ /$$__  $$| $$      | $$  | $$| $$  | $$
-| $$$$$$$$|  $$$$$$$|  $$$$$$$| $$      | $$  | $$|  $$$$$$/
-|________/ \_______/ \_______/|__/      |__/  |__/ \______/ """
-
 import pickle
 
-if __name__ == '__main__':
-    print(ascii)
-    print("")
-    print("Learn your formulas from notifications ;)")
-    print("")
+ascii = """     /$$                                                        
+    | $$                                                        
+    | $$        /$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$$   /$$$$$$ 
+    | $$       /$$__  $$ |____  $$ /$$__  $$| $$__  $$ /$$__  $$
+    | $$      | $$$$$$$$  /$$$$$$$| $$  \__/| $$  \ $$| $$  \ $$
+    | $$      | $$_____/ /$$__  $$| $$      | $$  | $$| $$  | $$
+    | $$$$$$$$|  $$$$$$$|  $$$$$$$| $$      | $$  | $$|  $$$$$$/
+    |________/ \_______/ \_______/|__/      |__/  |__/ \______/ """
 
-    Time_interval = int(input("How much notifiations you want to get per hour?: "))
+pc = """         ________________________________________________
+        /                                                \\
+       |    _________________________________________     |
+       |   |root@root:~#                             |    |
+       |   |    Choose your Desired Option:          |    |
+       |   |        0. Run Setup                     |    |
+       |   |        1. Import from txt               |    |
+       |   |                                         |    |
+       |   |    Use Presets: (Coming soon)           |    |
+       |   |        2. Derivatives                   |    |
+       |   |        3. trigonometric values          |    |
+       |   |                                         |    |
+       |   |                                         |    |
+       |   |                                         |    |
+       |   |                                         |    |
+       |   |Learn your formulas from notifications ;)|    |
+       |   |_________________________________________|    |
+       |                                                  |
+        \_________________________________________________/
+               \___________________________________/"""
+def DinoBino(Formulas):
+    Time = int(input("How much notifiations you want to get per hour?: "))
+    dic1 = {
+        "Formulas": Formulas,
+        "Time": Time
+    }
+
+    with open("E:\Python\codes\Learnig\config.bin","wb") as f:
+        pickle.dump(dic1,f)
+    
+    print(f"Loaded {len(Formulas)} Formulas and you will get {Time} Notifiations per hour, Happy learning.")
+
+def Setup():
     Number_of_Formulas = int(input("How much formulas you want to enter?: "))
     print("")
 
     Formulas = []
-
-    dic1 = {
-            "Formulas": Formulas,
-            "Time": Time_interval,
-            }
-
-
     for i in range(Number_of_Formulas):
         Formula = input(f"Enter formula {i+1}: ")
         Formulas.append(Formula)
 
-    with open("E:\Python\codes\Learnig\config.bin","wb") as f:
-        pickle.dump(dic1, f)
+    DinoBino(Formulas)
 
+def ImportFromFile(filelocation):
+    Formulas = []
+    with open(filelocation,"r") as f:
+        Formulas = f.readlines()
+    
+    test = []
+    for i in Formulas:
+        a = i.rstrip("\n")
+        test.append(a)
+
+    DinoBino(test)
+
+if __name__ == '__main__':
+    print(ascii)
+    print(pc) #nice
     print("")
-    print(f"Thanks for using Learno, You will get {Time_interval} notifications per hour and a total of {Number_of_Formulas} formulas. Enjoy learning :)")
+
+    while True:
+        select = int(input("Select your option (0/3): "))
+        
+        if select == 0:
+            Setup()
+            break
+        
+        elif select == 1:
+            print("Note: Keep 1 formula per line. please dont confuse my algorithm :P")
+            print("")
+
+            location = input("Enter File location (.txt): ")
+            ImportFromFile(location)
+
+            break
+
+        elif select == 2:
+            print("Coming soon..")
+            break
+
+        elif select == 3:
+            print("Coming soon..")
+            break
+
+        else:
+            print("Invalid Option :(")
